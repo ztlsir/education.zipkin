cccdfcbfc6ccbdac8f59536f140a359ab79dfae6
ztlsir <584821699@qq.com>
2020-06-06 16:18:36 +0800

    init porject

---
+++ src/main/java/com/ztlsir/zipkin/EducationZipkinApplication.java
@@ -0,0 +1,15 @@
+package com.ztlsir.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class EducationZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationZipkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ztlsir/zipkin/EducationZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztlsir.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
