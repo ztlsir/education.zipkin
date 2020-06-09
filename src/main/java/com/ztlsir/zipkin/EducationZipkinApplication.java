1b652c862951c3cb54980270e89be967bc926789
ztlsir <584821699@qq.com>
2020-06-09 10:03:09 +0800

    update zipkin version

--- src/main/java/com/ztlsir/zipkin/EducationZipkinApplication.java
+++ src/main/java/com/ztlsir/zipkin/EducationZipkinApplication.java
@@ -2,14 +2,12 @@ package com.ztlsir.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
 public class EducationZipkinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EducationZipkinApplication.class, args);
 	}
-
 }
